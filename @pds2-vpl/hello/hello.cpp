13993ddd5461b2afe5ee41a30647a2d5722830c5
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2024-03-06 20:48:34 -0300

    add hello question

---
+++ @pds2-vpl/hello/hello.cpp
@@ -0,0 +1,13 @@
+// Escreva um programa que apresente como saída o texto "Hello World!" em uma linha, e o seu número de matrícula na linha seguinte. 
+// (não coloque quebra de linha após a matrícula!). 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "2023028188";
+    
+    return 0;
+}
